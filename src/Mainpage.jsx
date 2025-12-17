ef7ec639dfb40e297d87bf3bbf735f868a8df371
Kevin Chang <keviny.chang@advantech.com>
2025-12-16 16:15:49 -0800

    update

--- src/Mainpage.jsx
+++ src/Mainpage.jsx
@@ -9,6 +9,27 @@ function Mainpage(props){
         <h2>{props.data.items[0].name}</h2>
         <h3>书目: {props.data.items[0].book}</h3>
         <h3>进度: <a href={props.data.items[0].link}>{props.data.items[0].chapter} {props.data.items[0].subject}</a></h3>
+        <h2>{props.data.items[1].name}</h2>
+        <h3>{props.data.items[1].books[0].name}:
+            <> <a href={props.data.items[1].link}>
+            {props.data.items[1].books[0].books.map((book)=>{
+                return <>{book.name} {book.chapters}</>
+            })}
+        </a></></h3>
+        <h3>{props.data.items[1].books[1].name}:
+            <> <a href={props.data.items[1].link}>
+            {props.data.items[1].books[1].books.map((book)=>{
+                return <> {book.name} {book.chapters}</>
+            })}
+        </a></></h3>
+        <h2>{props.data.items[2].name}</h2>
+        <h3>进度:
+            <> <a href={props.data.items[2].link}>
+            {props.data.items[2].books.map((book)=>{
+                return <> {book.name} {book.chapters}</>
+            })}
+        </a></></h3>
+        <h3>展览日期:  {props.data.items[2].date}</h3>
     </>;
 }
 
