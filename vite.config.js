3a7740549bfe79ec04ac837e0e175561bb650f84
Kevin Chang <keviny.chang@advantech.com>
2025-12-16 16:16:19 -0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Sets the base to /my-app/
-  //base: '/daily-bread/', // Sets the base to /my-app/
+  //base: '/', // Sets the base to /my-app/
+  base: '/daily-bread/', // Sets the base to /my-app/
   plugins: [react()],
 })
